a54f20fcaafd84001755d803e91f35b167aa7cee
John Joseph Mendez <johnjosephmendez2016@gmail.com>
2019-09-28 18:14:14 +0800

    added appbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TopAppBar from './components/layouts/appbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <TopAppBar text="Unionbank" exact path="/" />
 
+    </React.Fragment>
+  );
+};
 export default App;
